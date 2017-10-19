05b978aff4968fc5d200efc1e579f1580ea5c673
Vladimir Kostin <ollkostin@gmail.com>
2017-10-19 22:40:30 +0300

    working version

--- __main.py
+++ __main.py
@@ -1,26 +1,41 @@
 import csv
-from sklearn import metrics
-from sklearn.tree import DecisionTreeClassifier
+from sklearn import metrics, tree
+from subprocess import check_call
+from sklearn.tree import DecisionTreeClassifier, export_graphviz
 import numpy as np
 
+# read data from file
 with open('p/kredit_3.csv', 'r') as dest_f:
     data_iter = csv.reader(dest_f)
     data = [data for data in data_iter]
 data_array = np.asarray(data)
-x = data_array[1:, :]
-y = data_array[1:, 0]
-# print(x)
-# print(y)
+learnX = data_array[1:600, 1:21]
+learnY = data_array[1:600, 0]
 
+print(learnX)
+print(learnY)
 
 # fit a CART model to the data
-model = DecisionTreeClassifier()
-model.fit(x, y)
-print(model)
+learnModel = DecisionTreeClassifier()
+learnModel.fit(learnX, learnY)
 # make predictions
-expected = y
-predicted = model.predict(x)
+learnExpected = learnY
+learnPredicted = learnModel.predict(learnX)
 # summarize the fit of the model
-print(metrics.classification_report(expected, predicted))
-print(metrics.confusion_matrix(expected, predicted))
-# tree.export_graphviz(model, out_file='tree.dot')
+print(metrics.classification_report(learnExpected, learnPredicted))
+print(metrics.confusion_matrix(learnExpected, learnPredicted))
+
+# testing model
+testX = data_array[601:, 1:21]
+testY = data_array[601:, 0]
+
+# fit a CART model to the data
+learnModel.fit(testX, testY)
+
+testExpected = testY
+testPredicted = learnModel.predict(testX)
+print(metrics.classification_report(testExpected, testPredicted))
+print(metrics.confusion_matrix(testExpected, testPredicted))
+
+tree.export_graphviz(learnModel, 'learnModel.dot')
+check_call(['dot', '-Tpng', 'learnModel.dot', '-o', 'learnModel.png'])
