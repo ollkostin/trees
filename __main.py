6868f58c36317b0c831bcade97706bc980628a90
Vladimir Kostin <ollkostin@gmail.com>
2017-10-20 17:55:09 +0300

    final

--- __main.py
+++ __main.py
@@ -1,5 +1,5 @@
 import csv
-from sklearn import metrics, tree
+from sklearn import metrics, tree, model_selection
 from subprocess import check_call
 from sklearn.tree import DecisionTreeClassifier, export_graphviz
 import numpy as np
@@ -9,13 +9,14 @@ with open('p/kredit_3.csv', 'r') as dest_f:
     data_iter = csv.reader(dest_f)
     data = [data for data in data_iter]
 data_array = np.asarray(data)
-learnX = data_array[1:500, 1:21]
-learnY = data_array[1:500, 0]
-testX = data_array[502:, 1:21]
-testY = data_array[502:, 0]
+X = data_array[1:, 1:21]
+Y = data_array[1:, 0]
+
+learnX, testX, learnY, testY = model_selection.train_test_split(X, Y, train_size=0.20, random_state=100)
 
 # fit a CART model to the data
-model = DecisionTreeClassifier()
+model = DecisionTreeClassifier(max_leaf_nodes=6,  max_depth=3, min_samples_split=2, splitter="best",
+                               min_weight_fraction_leaf=0.)
 model.fit(learnX, learnY)
 # make predictions
 expected = testY
@@ -23,6 +24,7 @@ predicted = model.predict(testX)
 # summarize the fit of the model
 print(metrics.classification_report(expected, predicted))
 print(metrics.confusion_matrix(expected, predicted))
+print(metrics.accuracy_score(expected, predicted) * 100)
 
 tree.export_graphviz(model, 'tree.dot')
 check_call(['dot', '-Tpng', 'tree.dot', '-o', 'tree.png'])
--- __random_forest.py
+++ __random_forest.py
@@ -2,7 +2,7 @@ import csv
 from subprocess import check_call
 
 import numpy as np
-from sklearn import metrics
+from sklearn import metrics, model_selection
 from sklearn.ensemble import RandomForestClassifier
 # read data from file
 from sklearn.tree import export_graphviz
@@ -14,11 +14,13 @@ with open('p/kredit_3.csv', 'r') as dest_f:
 data_array = np.asarray(data)
 learnX = data_array[1:500, 1:21]
 learnY = data_array[1:500, 0]
-testX = data_array[502:, 1:21]
-testY = data_array[502:, 0]
+X = data_array[1:, 1:21]
+Y = data_array[1:, 0]
+
+learnX, testX, learnY, testY = model_selection.train_test_split(X, Y, train_size=0.20, random_state=100)
 
 # fit model to the data
-model = RandomForestClassifier(5)
+model = RandomForestClassifier(n_estimators=330, oob_score=True)
 model.fit(learnX, learnY)
 # make predictions
 expected = testY
@@ -27,10 +29,19 @@ predicted = model.predict(testX)
 
 print(metrics.accuracy_score(expected, predicted))
 print(metrics.confusion_matrix(expected, predicted))
+print(model.oob_score_ * 100)
 
-i = 1
-for m in model.estimators_:
-    prefix = 'forest_tree' + str(i)
-    export_graphviz(m, prefix + '.dot')
-    check_call(['dot', '-Tpng', prefix + '.dot', '-o', prefix + '.png'])
-    i += 1
+# i = 1
+# for i in range(1, 300):
+#     models = RandomForestClassifier(n_estimators=i, oob_score=True)
+#     expected = testY
+#     predicted = model.predict(testX)
+#     models.fit(learnX, learnY)
+#     print(str(i) + " " + str(model.oob_score_ * 100))
+
+# i = 1
+# for m in model.estimators_:
+#     prefix = 'forest_tree' + str(i)
+#     export_graphviz(m, prefix + '.dot')
+#     check_call(['dot', '-Tpng', prefix + '.dot', '-o', prefix + '.png'])
+#     i += 1
